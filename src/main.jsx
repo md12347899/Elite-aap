162f3ff874c8194a28463721d035f67a663eeeaf
md12347899 <wertodmahmud@gmail.com>
2026-06-30 05:53:50 +0300

    Set up ReactDOM rendering for NukhbaApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NukhbaApp from "./NukhbaApp.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NukhbaApp />
+  </React.StrictMode>
+);
