a74f6b9f3318cab6b7a7e9f317dfd66eea3513a7
md12347899 <wertodmahmud@gmail.com>
2026-07-06 09:02:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import EliteApp from "./EliteApp.jsx";
+import NukhbaApp from "./NukhbaApp.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><EliteApp /></React.StrictMode>
+  <React.StrictMode><NukhbaApp /></React.StrictMode>
 );
